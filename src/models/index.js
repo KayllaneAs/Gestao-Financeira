e017c32c3fe79fd1a24d0e05f41f71004fd7719c
lauraVasc <lauravasconcelos3001@gmail.com>
2026-06-17 17:53:12 -0300

    feat(banco): adiciona model ContaCartao e registra no index para US26

--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,15 @@
 import sequelize from '../config/database.js'
 import defineUsuario from './usuario.js'
+import defineContaCartao from './ContaCartao.js'
 
 const Usuario = defineUsuario(sequelize)
+const ContaCartao = defineContaCartao(sequelize)
 
-const db = { sequelize, Usuario }
+const db = { sequelize, Usuario, ContaCartao }
 
 Object.values(db).forEach((model) => {
   if (model?.associate) model.associate(db)
 })
 
 export default db
-export { sequelize, Usuario }
+export { sequelize, Usuario, ContaCartao }
