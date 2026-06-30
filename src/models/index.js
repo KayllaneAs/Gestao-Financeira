c82a6f4c97120e3619552db7a5dd2c4a813439d3
lauraVasc <lauravasconcelos3001@gmail.com>
2026-06-30 18:06:22 -0300

    feat(banco): adiciona models Despesa e ParcelamentoAgrupador e atualiza index para US14

--- src/models/index.js
+++ src/models/index.js
@@ -2,16 +2,20 @@ import sequelize from '../config/database.js'
 import defineUsuario from './usuario.js'
 import defineContaCartao from './ContaCartao.js'
 import defineRenda from './renda.js'
+import defineParcelamentoAgrupador from './ParcelamentoAgrupador.js'
+import defineDespesa from './Despesa.js'
 
 const Usuario = defineUsuario(sequelize)
 const ContaCartao = defineContaCartao(sequelize)
 const Renda = defineRenda(sequelize)
+const ParcelamentoAgrupador = defineParcelamentoAgrupador(sequelize)
+const Despesa = defineDespesa(sequelize)
 
-const db = { sequelize, Usuario, ContaCartao, Renda }
+const db = { sequelize, Usuario, ContaCartao, Renda, ParcelamentoAgrupador, Despesa }
 
 Object.values(db).forEach((model) => {
   if (model?.associate) model.associate(db)
 })
 
 export default db
-export { sequelize, Usuario, ContaCartao, Renda }
+export { sequelize, Usuario, ContaCartao, Renda, ParcelamentoAgrupador, Despesa }
